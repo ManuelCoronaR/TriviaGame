4cc35c8d74a766dd238aa7b3d7b52f8b8bf8a9f4
Manuel Corona <coronamvirgo@gmail.com>
2019-12-18 16:59:45 -0500

    finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,147 +1,126 @@
-// Global variables
-var time = 20;
-var questions = [
-    {
-        q1: ["What does the HTML stand for?"],
-        answers1: [{
-            1: "Hyper Text Tool Language",
-            2: "Text Markup Tool",
-            3: "Hyper Text Markup Language",//Correct Answer
-
-        }],
-
-        q2: ["What does the CSS stand for?"],
-        answers2: [{
-            a: "b2",
-            c: "d2",
-            e: "f2",
-            g: "h2"
-        }],
+$(document).ready(function() {
+    // Create a function that creates the start button and initial screen
+    
+    function initialScreen() {
+        startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>";
+        $(".mainArea").html(startScreen);
     }
-]
-
-
-var correctAnswers;
-var wrongAnswers;
-var intervalId;
-// Functions
-// =================================
-// Test
-console.log(questions.q2);
-console.log(answers.answers1[0][1]);
-
-function startGame() {
-}
-function gamePlay() {
-
-    $(".startBtn").on("click", function () {
-
-        // Hides the startBtn
-        $('.startBtn').hide();
-        timeCount();
-        quest1();
-        chooseCheckAnswer();
-    });
-}
-function timeCount() {
-
-    time = 7;
-
-    interval = setInterval(timeRemaining, 1000);
-
-    $('.btnContainer').append('<div class="timeRem">' + timeRemaining + '<div class="timerCountDown"></div>' + '</div>');
-}
-//Print Time remaining to the DOM and once it = 0 timer stops 
-
-function timeRemaining() {
-    time--; //Decrement time by 1 or time = time-1;
-
-    $(".timerCountDown").text(time + "seconds");
-    //Test / Debug
-    console.log(time);
-    // Oncetime reaches 0
-    if (time === 0) {
-        // Test/Debug
-        console.log('time = ' + time);
-        // Stops time at 0
-        clearInterval(interval);
-        // Removes button aswers
-        $("button").remove();
-        // Remove question
-        $('.questionInput').remove();
-        // Displays correct answer accordingly
-        disCorAnsw();
-
-    }
-}
-
-function disCorAnsw() {
-    //Display correct answer after 5 seconds and moves on to next question
-    if (qCounter === 1) {
-        var newDiv = $('<div class="corAns">');
-        correctAnswer = newDiv.text('Time is up' + asnwers.answers1[0][3]);
-        $('.btnContainer').append(correctAnswer);
-        //Removes the correct answer after 5 seconds and moves on to the next question 
-        setTimeout(function () {
-            $('div').remove('.corAnsw');
-            quest2();
-        }, 5000);
-    }
-}
-
-
-function quest1() {
-
-    qCounter++;
-    //Add questions to questionInput Div
-    $('.btnContainer').append('<div class="questionInput">' + questions.q1 + '</div>');
-    //Add answers
-    $('.btnContainer').append('<button class="answerVal" value=0>' + answer.answers1[0][1] + '</button>');
-    //Add answers
-    $('.btnContainer').append('<button class="answersVal" value=0>' + answer.answers1[0][2] + '</button>');
-    //Add answers
-    $('.btnContainer').append('<button class="answersVal" value=1>' + answer.answers1[0][3] + '</button>');
-
-}
-
-function quest2() {
-
-    qCounter++;
-    //Add questions to questionInput Div
-    $('.btnContainer').append('<div class="questionInput">' + questions.q2 + '</div>');
-    //Add answers
-    $('.btnContainer').append('<button class="answerVal" value=0>' + answer.answers2[0][1] + '</button>');
-    //Add answers
-    $('.btnContainer').append('<button class="answersVal" value=0>' + answer.answers2[0][2] + '</button>');
-    //Add answers
-    $('.btnContainer').append('<button class="answersVal" value=1>' + answer.answers2[0][3] + '</button>');
-
-}
-function chooseCheckAnswer() {
-    $('button').on('click', function () {
-        //Gets the value attribute of wich ever button is clicked
-        var checkRightAnswer = $(this).attr('value');
-        //Test / Debug 
-        console.log(checkRightAnswer);
-        //Stops the time when the right asnwers is selected 
-        clearInterval(interval);
-        //If the atrribute value of check answer is one...
-        if(checkRightAnswer == 1){
-            // Test/Debug
-            console.log('Correct');
-            //Removes button answer
-            $('button').remove();
-            //Remove question
-            $('.questionImput').remove();
+    
+    initialScreen();
+    
+    //Create a function, generateHTML(), that is triggered by the start button, and generates the HTML seen on the project video...
+    
+    $("body").on("click", ".start-button", function(event){
+        event.preventDefault();  // added line to test issue on GitHub Viewer
+        clickSound.play();
+        generateHTML();
+    
+        timerWrapper();
+    
+    }); // Closes start-button click
+    
+    $("body").on("click", ".answer", function(event){
+        //answeredQuestion = true;
+        clickSound.play();
+        selectedAnswer = $(this).text();
+        if(selectedAnswer === correctAnswers[questionCounter]) {
+            //alert("correct");
+    
+            clearInterval(theClock);
+            generateWin();
         }
         else {
-            //Removes button answers
-            $('button').remove();
-            //Remove question
-            $('.questionInput').remove();
-
+            //alert("wrong answer!");
+            clearInterval(theClock);
+            generateLoss();
         }
-
-    });
-
-}
+    }); // Close .answer click
+    
+    $("body").on("click", ".reset-button", function(event){
+        clickSound.play();
+        resetGame();
+    }); // Closes reset-button click
+    
+    });  //  Closes jQuery wrapper
+    
+    function generateLossDueToTimeOut() {
+        unansweredTally++;
+        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
+        $(".mainArea").html(gameHTML);
+        setTimeout(wait, 4000);  //  change to 4000 or other amount
+    }
+    
+    function generateWin() {
+        correctTally++;
+        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter] + "</p>" + imageArray[questionCounter];
+        $(".mainArea").html(gameHTML);
+        setTimeout(wait, 4000);  //  change to 4000 or other amount
+    }
+    
+    function generateLoss() {
+        incorrectTally++;
+        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
+        $(".mainArea").html(gameHTML);
+        setTimeout(wait, 4000); //  change to 4000 or other amount
+    }
+    
+    function generateHTML() {
+        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. "+answerArray[questionCounter][1]+"</p><p class='answer'>C. "+answerArray[questionCounter][2]+"</p><p class='answer'>D. "+answerArray[questionCounter][3]+"</p>";
+        $(".mainArea").html(gameHTML);
+    }
+    
+    function wait() {
+        if (questionCounter < 7) {
+        questionCounter++;
+        generateHTML();
+        counter = 30;
+        timerWrapper();
+        }
+        else {
+            finalScreen();
+        }
+    }
+    
+    function timerWrapper() {
+        theClock = setInterval(thirtySeconds, 1000);
+        function thirtySeconds() {
+            if (counter === 0) {
+                clearInterval(theClock);
+                generateLossDueToTimeOut();
+            }
+            if (counter > 0) {
+                counter--;
+            }
+            $(".timer").html(counter);
+        }
+    }
+    
+    function finalScreen() {
+        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
+        $(".mainArea").html(gameHTML);
+    }
+    
+    function resetGame() {
+        questionCounter = 0;
+        correctTally = 0;
+        incorrectTally = 0;
+        unansweredTally = 0;
+        counter = 30;
+        generateHTML();
+        timerWrapper();
+    }
+    
+    var startScreen;
+    var gameHTML;
+    var counter = 30;
+    var questionArray = ["What is the capital of Australia?", "What is the capital of Liberia?", "What is the capital of Taiwan?", "What is the capital of Japan?", "What is the capital of China?", "What is the capital of Turkey?", "What is the capital of Colombia?", "What is the capital of India?"];
+    var answerArray = [["Canberra", "Melbourne", "Sydney", "Darwin"], ["Arthington","Monrovia","Tuzon","Marshall"], ["Tainan City", "Taichung", "Taipei", "Hsinchu"], ["Kyoto","Hiroshima","Tokyo","Osaka"], ["Hong Kong", "Macau", "Shanghai", "Beijing"], ["Ankara","Istanbul","Antalya","Bursa"], ["Medellin", "Bogota", "Cartagena", "Cali"], ["Mumbai","Hyderabad","Bangalore","New Delhi"]];
+    var imageArray = ["<img class='center-block img-right' src='img/australia.png'>", "<img class='center-block img-right' src='img/liberia.png'>", "<img class='center-block img-right' src='img/taiwan.png'>", "<img class='center-block img-right' src='img/japan.png'>", "<img class='center-block img-right' src='img/china.png'>", "<img class='center-block img-right' src='img/turkey.png'>", "<img class='center-block img-right' src='img/colombia.png'>", "<img class='center-block img-right' src='img/india.png'>"];
+    var correctAnswers = ["A. Canberra", "B. Monrovia", "C. Taipei", "C. Tokyo", "D. Beijing", "A. Ankara", "B. Bogota", "D. New Delhi"];
+    var questionCounter = 0;
+    var selecterAnswer;
+    var theClock;
+    var correctTally = 0;
+    var incorrectTally = 0;
+    var unansweredTally = 0;
+    var clickSound = new Audio("sound/button-click.mp3");
